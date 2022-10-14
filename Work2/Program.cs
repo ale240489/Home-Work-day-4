34265b050537839b545909eb2a8f0c1834dd46b5
Aleksandr <ale240489@yandex.ru>
2022-10-14 14:50:30 +0300

    Work 2

---
+++ Work2/Program.cs
@@ -0,0 +1,25 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+int GetNumber()
+{
+    Console.WriteLine("Введите число");
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int SummOfNumber(int a)
+{
+    string sum = a.ToString();
+    int summa = 0;
+    for(int i = 0; i < sum.Length; i++)
+    {
+        summa = summa + int.Parse(sum[i].ToString());
+    }
+    return summa;
+}
+
+int y = GetNumber();
+Console.WriteLine ($"Сумма цифр в числе {y} = {SummOfNumber(y)}");
