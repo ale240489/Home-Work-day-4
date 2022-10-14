9b479e847f5530c7ad9734a1c1c6b8d408d7a166
Aleksandr <ale240489@yandex.ru>
2022-10-14 14:51:11 +0300

    Work 3

---
+++ Work3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+//ввод элеменетов через консоль (через запятую. использовать метод string.Split())
+Console.WriteLine("Введите последобательно 8 любых чисел, через запятую, без пробелов");
+int [] array = new int[8]; 
+string [] arrayNum = Console.ReadLine().Split(',');
+Console.WriteLine("Получившийся массив :");
+for(int i = 0; i < array.Length; i++)
+{
+   array[i] = int.Parse(arrayNum[i]);
+}
+for(int i = 0; i < array.Length; i++)
+{
+    Console.Write ($"{array[i]} ");
+}
